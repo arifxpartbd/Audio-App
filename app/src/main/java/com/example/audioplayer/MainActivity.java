a35f13a4b63d5127288bc38d4276f46dd5e0f040
ARIF <arifkhancsebd@gmail.com>
2020-12-20 00:12:54 +0600

    Button added

--- app/src/main/java/com/example/audioplayer/MainActivity.java
+++ app/src/main/java/com/example/audioplayer/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.audioplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button gitbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //mack a new file
+
+        gitbutton=findViewById(R.id.new_gitId);
+
     }
 }
